0738cac0297caae8264e434444fc91a795070dec
thaynarlt <tatarlt7@gmail.com>
2023-08-29 19:37:28 -0300

    .

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,14 @@
+public class Operadores {
+    public static void main(String[] args) {
+        int numero = 5;
+        System.out.println(- numero);
+        System.out.println(numero);
+
+
+
+
+
+
+
+    }
+}
--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,15 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        String concatenacao ="?"; 
+        concatenacao = 1+1+1+"1";
+        System.out.println(concatenacao);
+        concatenacao = 1+"1"+1+1;
+        System.out.println(concatenacao);
+        concatenacao = 1+"1"+1+"1";
+        System.out.println(concatenacao);
+        concatenacao = "1"+1+1+1;
+        System.out.println(concatenacao);
+        concatenacao = "1"+(1+1+1);
+        System.out.println(concatenacao);
     }
 }
