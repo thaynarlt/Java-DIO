c16344b0e54fc0e75948fd1da1929d05ff0279d5
thaynarlt <tatarlt7@gmail.com>
2023-08-29 20:12:32 -0300

    .

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,12 +1,12 @@
 public class Operadores {
     public static void main(String[] args) {
         int numero = 5;
-        System.out.println(- numero);
+        numero = - numero;
         System.out.println(numero);
-
-
-
-
+        numero = + numero; //ficou (-5) porque é necessário que seja (* -1)
+        System.out.println(numero);
+        numero = numero * -1;
+        System.out.println(numero);
 
 
 
--- tipos-variaveis/src/TiposDeVariaveis.java
+++ tipos-variaveis/src/TiposDeVariaveis.java
@@ -1,5 +1,6 @@
-public class TiposVariaveis {
+public class TiposDeVariaveis {
     public static void main(String[] args) throws Exception {
+
         String concatenacao ="?"; 
         concatenacao = 1+1+1+"1";
         System.out.println(concatenacao);
