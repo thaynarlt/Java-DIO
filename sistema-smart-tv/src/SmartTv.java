1b6282578747fb1dd33b280a7fd0ee9cf1515eee
thaynarlt <tatarlt7@gmail.com>
2023-08-30 09:45:54 -0300

    Commit

--- sistema-smart-tv/src/SmartTv.java
+++ sistema-smart-tv/src/SmartTv.java
@@ -2,4 +2,22 @@ public class SmartTv {
     boolean ligada = false;
     int canal =1;
     int volume = 25;
+
+    public void aumentarVolume(){
+        volume ++; // volume = volume +1
+    }
+    public void diminuirVolume(){
+        volume --; // volume = volume -1
+    }
+
+
+
+
+
+    public void ligar(){
+        ligada=true;
+    }
+    public void desligar(){
+        ligada=true;
+    }
 }
--- sistema-smart-tv/src/Usuario.java
+++ sistema-smart-tv/src/Usuario.java
@@ -1,8 +1,24 @@
 public class Usuario {
     public static void main(String[] args) throws Exception {
        SmartTv smartTv = new SmartTv();
+
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.aumentarVolume();
+        System.out.println("Volume Atual: " +smartTv.volume);
+
+
        System.out.println("TV Ligada? " +smartTv.ligada);
        System.out.println("Canal Atual: " +smartTv.canal);
-       System.out.println("Volume Atual: " +smartTv.canal);
+
+
+
+
+
+       smartTv.ligar();
+       System.out.println("Novo Status -> TV Ligada?" + smartTv.ligada);
+       smartTv.desligar();
+       System.out.println("Novo Status -> TV Ligada?" + smartTv.ligada);
     }
 }
