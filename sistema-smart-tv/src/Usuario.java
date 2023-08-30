8f62844e2eb9c41840b3a14aff8cb37c20d03973
Thayná Tolentino <75785465+thaynarlt@users.noreply.github.com>
2023-08-30 09:39:40 -0300

    .

---
+++ sistema-smart-tv/src/Usuario.java
@@ -0,0 +1,8 @@
+public class Usuario {
+    public static void main(String[] args) throws Exception {
+       SmartTv smartTv = new SmartTv();
+       System.out.println("TV Ligada? " +smartTv.ligada);
+       System.out.println("Canal Atual: " +smartTv.canal);
+       System.out.println("Volume Atual: " +smartTv.canal);
+    }
+}
