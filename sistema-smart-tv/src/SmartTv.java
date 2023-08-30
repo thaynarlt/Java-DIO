68d4b2537f2597f91bbd7edcbbffb19be8b375bc
thaynarlt <tatarlt7@gmail.com>
2023-08-30 09:51:17 -0300

    .

--- sistema-smart-tv/src/SmartTv.java
+++ sistema-smart-tv/src/SmartTv.java
@@ -3,11 +3,26 @@ public class SmartTv {
     int canal =1;
     int volume = 25;
 
+
+    public void mudarCanal(int novoCanal){
+        canal = novoCanal;
+    }
+    public void subirCanal(){
+        canal++;
+    }
+    public void descerCanal(){
+        canal--;
+    }
+
+
+
     public void aumentarVolume(){
         volume ++; // volume = volume +1
+        System.out.println("Aumentando o volume para: " + volume);
     }
     public void diminuirVolume(){
         volume --; // volume = volume -1
+        System.out.println("Aumentando o volume para: " + volume);
     }
 
 
--- sistema-smart-tv/src/Usuario.java
+++ sistema-smart-tv/src/Usuario.java
@@ -9,9 +9,13 @@ public class Usuario {
         System.out.println("Volume Atual: " +smartTv.volume);
 
 
-       System.out.println("TV Ligada? " +smartTv.ligada);
-       System.out.println("Canal Atual: " +smartTv.canal);
+        smartTv.mudarCanal(13);
+        System.out.println("Canal Atual: " +smartTv.canal);
 
+
+        System.out.println("TV Ligada? " +smartTv.ligada);
+        
+    
 
 
 
