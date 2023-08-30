78a049f12b798d4c6285e3e6ef7293b0a5c222b5
thaynarlt <tatarlt7@gmail.com>
2023-08-29 21:10:01 -0300

    .

---
+++ tipos-variaveis/src/Metodos.java
@@ -0,0 +1,12 @@
+public class Metodos {
+    public static void main(String[] args) {
+        //Seguir o padrão camelCase:
+        /*
+        somar(int n1, int n2){}
+        abrirConexao(){}
+        concluirProcessamento() {}
+        findById(int id){} // não se assuste, você verá muito método em inglês em sua jornada
+        calcularImprimir(){} // há algo de errado neste método, ele deveria ter uma única finalidade
+        */
+    }
+}
