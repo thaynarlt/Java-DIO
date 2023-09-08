b4ae71b1b1c9e3c2a80e761a9a87ac1af75e2055
Thayna Tolentino <tatarlt7@gmail.com>
2023-09-08 08:09:25 -0300

    commit

--- controle-candidatos/src/candidatura/ProcessoSeletivo.java
+++ controle-candidatos/src/candidatura/ProcessoSeletivo.java
@@ -1,20 +1,65 @@
 package candidatura;
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 public class ProcessoSeletivo {
     public static void main(String[] args){
-        selecaoCandidatos();
-        //analisarCandidato(1900.0);
-        //analisarCandidato(2200.0);
-        //analisarCandidato(2000.0);
-
+        String [] candidatos = {"FELIPE","MÁRCIA","JULIA","PAULO","AUGUSTO"};
+        for(String candidato: candidatos) {
+            entrandoEmContato(candidato);
+        }
     }
+
+    static void entrandoEmContato(String candidato){
+        int tentativasRealizadas =1;
+        boolean continuarTentando = true;
+        boolean atendeu=false;
+        do {
+            atendeu= atender();
+			continuarTentando = !atendeu;
+			if(continuarTentando)
+				tentativasRealizadas++;
+			else
+				System.out.println("CONTATO REALIZADO COM SUCESSO");
+                
+        }while(continuarTentando && tentativasRealizadas<3);
+
+        if(atendeu)
+			System.out.println("CONSEGUIMOS CONTATO COM " + candidato +" NA " + tentativasRealizadas + " TENTATIVA");
+		else
+			System.out.println("NÃO CONSEGUIMOS CONTATO COM " + candidato +", NÚMERO MAXIMO TENTATIVAS " + tentativasRealizadas + " REALIZADA");
+    }
+
+
+
+
+    //método auxiliar
+    static boolean atender(){
+        return new Random().nextInt(3)==1;
+    }
+
+
+
+    static void imprimirSelecionados() {
+        String [] candidatos = {"FELIPE","MÁRCIA","JULIA","PAULO","AUGUSTO"};
+        System.out.println("Imprimindo a lista de candidatos informando o índice do elemento");
+        
+        for(int indice =0; indice < candidatos.length;indice++) {
+            System.out.println("Candidato de nº "+(indice+1)+ " é "+candidatos[indice]);
+        }
+        System.out.println("Forma abreviada de interação for each:");
+        for(String candidato: candidatos){
+            System.out.println("O candidato selecionado foi "+candidato);
+        }
+    }
+
+
     static void selecaoCandidatos() {
         // Array com a lista de candidatos
-        String [] candidatos = {"FELIPE","MÁRCIA","JULIA","PAULO","AUGUSTO","MÔNICA","FABRÍCIO","MIRELA","DANIELA","JORGE"};
+        String [] candidatos = {"FELIPE","MÁRCIA","JULIA","PAULO","AUGUSTO"};
         int candidatosSelecionados =0;
         int candidatoAtual =0;
         double salarioBase = 2000.0;
-        while(candidatosSelecionados<5);{
+        while(candidatosSelecionados<5 && candidatoAtual < candidatos.length);{
             String candidato = candidatos[candidatoAtual];
             double salarioPretendido = valorPretendido();
 
