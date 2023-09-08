ef2c274bbfe700105eb9994f5b7a64a843dc16cb
Thayna Tolentino <tatarlt7@gmail.com>
2023-09-08 09:38:02 -0300

    .

---
+++ linguagens-exercicios/desafio-percentual/src/Desafio.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Desafio {
+
+    public static void main(String[] args){
+        //Lê os valores de Entrada:
+        Scanner leitorDeEntradas = new Scanner(System.in);
+        float valorSalario = leitorDeEntradas.nextFloat();
+        float valorBeneficios = leitorDeEntradas.nextFloat();
+
+        float valorImposto = 0;
+        if (valorSalario >= 0 && valorSalario <= 1100){
+            //Atribui a aliquota de 5% medianete o salário: 
+            valorImposto = 0.05F * valorSalario;
+        } else if (valorSalario >= 1100.01 && valorSalario <= 2500.00){ 
+            valorImposto = 0.10F * valorSalario;
+        } else {
+            valorImposto = 0.15F * valorSalario;
+        }
+        //TODO Criar as demais condições para as aliquotas de 10.00% e 15.00%
+
+        //Calcula e imprime a Saída (com 2 casas decimais):
+        float saida = valorSalario - valorImposto + valorBeneficios;
+        System.out.println(String.format("%.2f", saida));
+    }
+}
