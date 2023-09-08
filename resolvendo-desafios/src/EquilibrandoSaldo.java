4f2f9b780d60b761516182d10e7de569800e2722
Thayna Tolentino <tatarlt7@gmail.com>
2023-09-08 11:23:46 -0300

    .

---
+++ resolvendo-desafios/src/EquilibrandoSaldo.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class EquilibrandoSaldo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double saldoAtual = scanner.nextDouble();
+        double valorDeposito = scanner.nextDouble();
+        double valorRetirada = scanner.nextDouble();
+
+       //TODO: Calcular o saldo atualizado de acordo com a descrição deste desafio.
+        double saldoAtualizado = (valorDeposito + saldoAtual) - valorRetirada;
+
+    //TODO: Imprimir o a saída de conforme a tabela de exemplos (uma casa decimal).
+    System.out.printf("Saldo atualizado na conta: "+ "%.1f%n", saldoAtualizado);
+    }
+}
