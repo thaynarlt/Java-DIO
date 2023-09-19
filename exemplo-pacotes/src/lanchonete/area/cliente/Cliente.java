19c5231ea938544eda96cdf6ffe5e21743ccdb54
Thayna Tolentino <tatarlt7@gmail.com>
2023-09-19 08:40:10 -0300

    commit

--- exemplo-pacotes/src/lanchonete/area/cliente/Cliente.java
+++ exemplo-pacotes/src/lanchonete/area/cliente/Cliente.java
@@ -8,12 +8,10 @@ public class Cliente {
 		System.out.println("FAZENDO O PEDIDO");
 	}
 	public void pagarConta() {
+		consultarSaldoAplicativo();
 		System.out.println("PAGANDO A CONTA");
 	}
-	public void consultarSaldoAplicativo() {
+	private void consultarSaldoAplicativo() {
 		System.out.println("CONSULTANDO SALDO NO APLICATIVO");
-	}
-	public void pegarPedidoBalcao() {
-		System.out.println("PEGANDO O PEDIDO NO BALCÃO");
 	}
 }
--- exemplo-pacotes/src/lanchonete/atendimento/Atendente.java
+++ exemplo-pacotes/src/lanchonete/atendimento/Atendente.java
@@ -2,19 +2,19 @@ package lanchonete.atendimento;
 
 public class Atendente {
 	public void servindoMesa() {
-		//...?
+		pegarLancheCozinha();
 		System.out.println("SERVINDO A MESA");
 	}
-	public void pegarLancheCozinha() {
+	private void pegarLancheCozinha() {
 		System.out.println("PEGANDO O LANCHE NA COZINHA");
 	}
 	public void receberPagamento() {
 		System.out.println("RECEBENDO PAGAMENTO");
 	}
-	public void trocarGas() {
+	void trocarGas() {
 		System.out.println("ATENDENTE TROCANDO O GÁS");
 	}
-	public void pegarPedidoBalcao() {
+	private void pegarPedidoBalcao() {
 		System.out.println("PEGANDO O PEDIDO NO BALCÃO");
 	}
 }
