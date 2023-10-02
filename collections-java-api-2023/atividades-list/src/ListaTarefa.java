32444deb517b9e6512e5401072bdb947430fac99
Thayna Tolentino <tatarlt7@gmail.com>
2023-10-02 11:11:38 -0300

    .

---
+++ collections-java-api-2023/atividades-list/src/ListaTarefa.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListaTarefa {
+    //Atributo
+    private List<Tarefa> tarefaList;
+
+    public ListaTarefa() {
+        this.tarefaList = new ArrayList<>();
+    }
+
+    //Método de Adicionar
+    public void adicionarTarefa(String descricao){
+        tarefaList.add(new Tarefa(descricao));
+    }
+
+    public void removerTarefa(String descricao){
+        List<Tarefa> tarefasParaRemover = new ArrayList<>();
+        for(Tarefa t : tarefaList){
+            if(t.getDescricao().equalsIgnoreCase(descricao)){
+                tarefasParaRemover.add(t);
+            }
+        }
+        tarefaList.removeAll((tarefasParaRemover));
+    }
+
+
+    public int obterNumeroTotalTarefas(){
+        return tarefaList.size();
+    }
+
+    public void obterDescricoesTarefas(){
+       System.out.println(tarefaList); 
+    }
+
+}
---
+++ collections-java-api-2023/atividades-list/src/Tarefa.java
@@ -0,0 +1,15 @@
+
+public class Tarefa {
+    //Atributo
+    private String descricao;
+
+    public Tarefa(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+
+}
